8736219be359ad810f07ed5864fd550341b15409
kosya <alexkositcin@gmail.com>
2016-10-23 19:29:19 +0300

    Добавил Main Menu

---
+++ The Information Man/The Information Man/Assets/Scripts/MainMenu.js
@@ -0,0 +1,51 @@
+﻿var Play = false;
+var Settings = false;
+var Quit = false;
+
+var camera1 : Camera;
+var camera2 : Camera;
+
+var Back = false;
+var Low = false;
+var Medium = false;
+var High = false;
+var Fantastic = false;
+
+function OnMouseEnter() {
+    GetComponent.<Renderer>().material.color = Color.red;
+}
+
+function OnMouseExit() {
+    GetComponent.<Renderer>().material.color = Color.white;
+}
+
+function OnMouseUp() {
+    if (Play) {
+        Application.LoadLevel(1);
+    }
+    if (Settings) {
+        camera1.enabled = false;
+        camera2.enabled = true;
+    }
+    if (Quit == true) {
+        Application.Quit();
+    }
+
+    //=============
+    if (Back == true) {
+        camera1.enabled = true;
+        camera2.enabled = false;
+    }
+    if (Low == true) {
+        QualitySettings.currentLevel = QualityLevel.Simple;
+    }
+    if (Medium == true) {
+        QualitySettings.currentLevel = QualityLevel.Good;
+    }
+    if (High == true) {
+        QualitySettings.currentLevel = QualityLevel.Beautiful;
+    } 
+    if (Fantastic == true) {
+        QualitySettings.currentLevel = QualityLevel.Fantastic;
+    }
+}
