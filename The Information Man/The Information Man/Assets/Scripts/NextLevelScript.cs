f9d4cbc29509c67af2287fcd253f9e835c7c11b0
Artur Khayaliev <a.khayaliev@innopolis.ru>
2016-10-30 21:27:11 +0300

    Collider on next level

--- The Information Man/The Information Man/Assets/Scripts/NextLevelScript.cs
+++ The Information Man/The Information Man/Assets/Scripts/NextLevelScript.cs
@@ -3,13 +3,11 @@ using System.Collections;
 
 public class NextLevelScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.name == "player")
+        {
+            Application.LoadLevel(1);
+        }
+    }
 }
