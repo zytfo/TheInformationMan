e2b6c67401d6aef14a769add11117341d8bc2816
kosya <alexkositcin@gmail.com>
2016-10-30 22:00:01 +0300

    Dialogue(Behining)

---
+++ The Information Man/The Information Man/Assets/Scripts/Dialogue.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Dialogue : MonoBehaviour {
+	public GameObject leftPanel;
+	public GameObject rightPanel;
+	public GameObject textPanel;
+	public GameObject inputField;
+	public Sprite leftPicture;
+	public Sprite rightPicture;
+
+
+	void Start () {
+		leftPanel.GetComponent<UnityEngine.UI.Image> ().sprite = leftPicture;
+		rightPanel.GetComponent<UnityEngine.UI.Image> ().sprite = rightPicture;
+	}
+}
