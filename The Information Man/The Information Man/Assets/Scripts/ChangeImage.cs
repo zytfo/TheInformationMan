fcc34274dd756db8adf2c5835d8b0bacb7d744b4
Artur Khayaliev <a.khayaliev@innopolis.ru>
2016-11-04 14:55:15 +0300

    Changes

---
+++ The Information Man/The Information Man/Assets/Scripts/ChangeImage.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ChangeImage : MonoBehaviour {
+
+    //public Image image;
+	// Use this for initialization
+	/*void Start () {
+        image.sprite = Resources.Load<Sprite>("Preview/dolj") as Sprite;
+	}*/
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //public void ChangeImage(string imageName)
+    //{
+        //image.sprite = Resources.Load<Sprite>("Preview/" + imageName) as Sprite;
+    //}
+}
