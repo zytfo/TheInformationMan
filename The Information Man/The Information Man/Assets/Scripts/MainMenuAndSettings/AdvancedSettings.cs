d42b63a77b15600919c6c9042d8f24e81b098359
kosya <alexkositcin@gmail.com>
2016-11-08 00:26:27 +0300

    Settings menu is фукен реди

--- The Information Man/The Information Man/Assets/Scripts/MainMenuAndSettings/AdvancedSettings.cs
+++ The Information Man/The Information Man/Assets/Scripts/MainMenuAndSettings/AdvancedSettings.cs
@@ -20,12 +20,16 @@ public class AdvancedSettings : MonoBehaviour {
 	public bool FXAA = false;
 	public bool MSAA = false;
 	public bool antialiasing = false;
-	public bool alfaBlending = false;
+	public bool alphaBlending = false;
 	public bool superSampling = false;
 
 	public GameObject spotLight1;
 	public GameObject spotLight2;
 	public GameObject spotLight3;
+
+	public GameObject menuText;
+
+	public GameObject grassObject;
 
 	public void OnMouseEnter() {
 		this.GetComponent<Renderer>().material.color = Color.red;
@@ -97,6 +101,195 @@ public class AdvancedSettings : MonoBehaviour {
 				spotLight3.GetComponent<Light> ().color = Color.red;
 			}
 		}
+
+		if (mode3D) {
+			string str = this.GetComponent<TextMesh> ().text;
+			if (str == "On") {
+				this.GetComponent<TextMesh> ().text = "Off";
+				menuText.GetComponent<Transform> ().rotation = Quaternion.Euler(0, 0, 0);
+			}
+			if (str == "Off") {
+				this.GetComponent<TextMesh> ().text = "On";
+
+				menuText.GetComponent<Transform> ().rotation = Quaternion.Euler(20, -20, -20);
+			}
+		}
+
+		if (smoothing) {
+			string str = this.GetComponent<TextMesh> ().text;
+			if (str == "On") {
+				this.GetComponent<TextMesh> ().text = "Off";
+			} else {
+				this.GetComponent<TextMesh> ().text = "On";
+			}
+		}
+
+		if (textures) {
+			string str = this.GetComponent<TextMesh> ().text;
+			if (str == "High") {
+				this.GetComponent<TextMesh> ().text = "Ultra";
+				QualitySettings.currentLevel = QualityLevel.Fantastic;
+			}
+			if (str == "Ultra") {
+				this.GetComponent<TextMesh> ().text = "Low";
+				QualitySettings.currentLevel = QualityLevel.Simple;
+			}
+			if (str == "Low") {
+				this.GetComponent<TextMesh> ().text = "Average";
+				QualitySettings.currentLevel = QualityLevel.Good;
+			}
+			if (str == "Average") {
+				this.GetComponent<TextMesh> ().text = "High";
+				QualitySettings.currentLevel = QualityLevel.Beautiful;
+			}
+		}
+
+		if (polygones) {
+			string str = this.GetComponent<TextMesh> ().text;
+			if (str == "High") {
+				this.GetComponent<TextMesh> ().text = "Ultra";
+			}
+			if (str == "Ultra") {
+				this.GetComponent<TextMesh> ().text = "Low";
+			}
+			if (str == "Low") {
+				this.GetComponent<TextMesh> ().text = "Average";
+			}
+			if (str == "Average") {
+				this.GetComponent<TextMesh> ().text = "High";
+			}
+		}
+
+		if (effects) {
+			string str = this.GetComponent<TextMesh> ().text;
+			if (str == "High") {
+				this.GetComponent<TextMesh> ().text = "Ultra";
+			}
+			if (str == "Ultra") {
+				this.GetComponent<TextMesh> ().text = "Low";
+			}
+			if (str == "Low") {
+				this.GetComponent<TextMesh> ().text = "Average";
+			}
+			if (str == "Average") {
+				this.GetComponent<TextMesh> ().text = "High";
+			}
+		}
+
+		if (postprocessing) {
+			string str = this.GetComponent<TextMesh> ().text;
+			if (str == "High") {
+				this.GetComponent<TextMesh> ().text = "Ultra";
+			}
+			if (str == "Ultra") {
+				this.GetComponent<TextMesh> ().text = "Low";
+			}
+			if (str == "Low") {
+				this.GetComponent<TextMesh> ().text = "Average";
+			}
+			if (str == "Average") {
+				this.GetComponent<TextMesh> ().text = "High";
+			}
+		}
+
+		if (grid) {
+			string str = this.GetComponent<TextMesh> ().text;
+			if (str == "High") {
+				this.GetComponent<TextMesh> ().text = "Ultra";
+			}
+			if (str == "Ultra") {
+				this.GetComponent<TextMesh> ().text = "Low";
+			}
+			if (str == "Low") {
+				this.GetComponent<TextMesh> ().text = "Average";
+			}
+			if (str == "Average") {
+				this.GetComponent<TextMesh> ().text = "High";
+			}
+		}
+
+		if (environment) {
+			string str = this.GetComponent<TextMesh> ().text;
+			if (str == "High") {
+				this.GetComponent<TextMesh> ().text = "Ultra";
+			}
+			if (str == "Ultra") {
+				this.GetComponent<TextMesh> ().text = "Low";
+			}
+			if (str == "Low") {
+				this.GetComponent<TextMesh> ().text = "Average";
+			}
+			if (str == "Average") {
+				this.GetComponent<TextMesh> ().text = "High";
+			}
+		}
+
+		if (grass) {
+			string str = this.GetComponent<TextMesh> ().text;
+			grassObject.SetActive(true);
+			if (str == "High") {
+				this.GetComponent<TextMesh> ().text = "Ultra";
+				grassObject.GetComponent<SpriteRenderer>().color = Color.green;
+			}
+			if (str == "Ultra") {
+				this.GetComponent<TextMesh> ().text = "Low";
+				grassObject.GetComponent<SpriteRenderer>().color = Color.black;
+			}
+			if (str == "Low") {
+				this.GetComponent<TextMesh> ().text = "Average";
+				grassObject.GetComponent<SpriteRenderer>().color = Color.gray;
+			}
+			if (str == "Average") {
+				this.GetComponent<TextMesh> ().text = "High";
+				grassObject.GetComponent<SpriteRenderer>().color = Color.yellow;
+			}
+		}
+
+		if (FXAA) {
+			string str = this.GetComponent<TextMesh> ().text;
+			if (str == "On") {
+				this.GetComponent<TextMesh> ().text = "Off";
+			} else {
+				this.GetComponent<TextMesh> ().text = "On";
+			}
+		}
+
+		if (MSAA) {
+			string str = this.GetComponent<TextMesh> ().text;
+			if (str == "On") {
+				this.GetComponent<TextMesh> ().text = "Off";
+			} else {
+				this.GetComponent<TextMesh> ().text = "On";
+			}
+		}
+
+		if (antialiasing) {
+			string str = this.GetComponent<TextMesh> ().text;
+			if (str == "On") {
+				this.GetComponent<TextMesh> ().text = "Off";
+			} else {
+				this.GetComponent<TextMesh> ().text = "On";
+			}
+		}
+
+		if (alphaBlending) {
+			string str = this.GetComponent<TextMesh> ().text;
+			if (str == "On") {
+				this.GetComponent<TextMesh> ().text = "Off";
+			} else {
+				this.GetComponent<TextMesh> ().text = "On";
+			}
+		}
+
+		if (superSampling) {
+			string str = this.GetComponent<TextMesh> ().text;
+			if (str == "On") {
+				this.GetComponent<TextMesh> ().text = "Off";
+			} else {
+				this.GetComponent<TextMesh> ().text = "On";
+			}
+		}
+
 	}
 }
 
